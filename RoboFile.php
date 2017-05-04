ee1387ff4a5e3002cdfe9ef9739196214ef6efdd
Josh <josh@kaiju.net>
2017-05-04 15:45:09 -0400

    Added packaging to Robofile

--- RoboFile.php
+++ RoboFile.php
@@ -9,6 +9,28 @@ class RoboFile extends \Robo\Tasks
     // define public methods as commands
     public function build()
     {
+
         $this->say("Build :)");
+
+        $collection = $this->collectionBuilder();
+
+        $temp_dir = $collection->tmpDir();
+
+        $collection
+            ->taskRsync()
+                ->fromPath([
+                    __DIR__ . '/app'
+                    ])
+                ->toPath($temp_dir)
+                ->recursive()
+            ->taskFilesystemStack()
+                ->mkdir(__DIR__ . '/build')
+            ->taskPack(__DIR__ . '/build/package.tar.gz')
+                ->addDir('my_thing', $temp_dir);
+
+        $collection->run();
+
+        $this->say("Build done.");
+
     }
 }
